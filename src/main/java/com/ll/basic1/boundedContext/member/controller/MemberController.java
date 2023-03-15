634b70ced43f8dc6728c01d8bb9efecc037665f7
heeyeon3050 <kho200a@naver.com>
2023-03-15 13:11:23 +0900

    update

---
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.ll.basic1.boundedContext.member.controller;
+
+import com.ll.basic1.base.rsData.RsData;
+import com.ll.basic1.boundedContext.member.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Controller
+public class MemberController {
+    private MemberService memberService;
+
+    public MemberController() {
+        memberService = new MemberService();
+    }
+
+    @GetMapping("/member/login")
+    @ResponseBody
+    public RsData login(String username, String password){
+        if ( username == null || username.trim().length() == 0 ) {
+            return RsData.of("F-3", "username(을)를 입력해주세요.");
+        }
+
+        if ( password == null || password.trim().length() == 0 ) {
+            return RsData.of("F-4", "password(을)를 입력해주세요.");
+        }
+
+        return memberService.tryLogin(username, password);
+    }
+}
---
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.ll.basic1.boundedContext.member.service;
+
+import com.ll.basic1.base.rsData.RsData;
+import com.ll.basic1.boundedContext.member.entity.Member;
+import com.ll.basic1.boundedContext.member.repository.MemberRepository;
+
+public class MemberService {
+    private MemberRepository memberRepository;
+
+    public MemberService() {
+        memberRepository = new MemberRepository();
+    }
+
+    public RsData tryLogin(String username, String password) {
+        Member member = memberRepository.findByUsername(username);
+
+        if(member == null){
+            return RsData.of("F-1", username + "(은)는 존재하지 않는 회원입니다.");
+        }
+        else if(!member.getPassword().equals(password)){
+            return RsData.of("F-2", "비밀번호가 일치하지 않습니다.");
+        }
+
+        return RsData.of("S-1", username + " 님 환영합니다.");
+    }
+}
