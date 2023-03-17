48a96206544b13b12726e65741e93e0f4ef62061
heeyeon3050 <kho200a@naver.com>
2023-03-17 15:50:46 +0900

    서비스, 레포지토리 도입 & 등록날짜, 수정날짜 자동갱신

---
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -0,0 +1,32 @@
+package com.ll.basic1.boundedContext.article.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Article {
+    @Id //Primary Key
+    @GeneratedValue(strategy = IDENTITY) //AUTO_INCREMENT
+    private long id;
+    @CreatedDate
+    private LocalDateTime createDate; //데이터 생성 날짜
+    @LastModifiedDate
+    private LocalDateTime modifyDate; //데이터 수정 날짜
+    private String title;
+    private String body;
+}
